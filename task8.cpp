45fdc278ced91b07615c8b1c331e23a3c4f6ef15
abdulrahmon08 <abdulrahmonsheriff08@gmail.com>
2026-04-07 21:27:02 +0100

    update

---
+++ task8.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string firstName = "Abdulrahmon";
+    string lastName = "Sheriff";
+    string fullName = firstName + " " + lastName;
+    cout << "Full Name: " << fullName;
+    return 0;
+}
