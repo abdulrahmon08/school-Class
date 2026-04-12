54d2863b709b4363734fe1a2be94e05e2781d449
abdulrahmon08 <abdulrahmonsheriff08@gmail.com>
2026-04-12 13:05:24 +0100

    update

---
+++ task12.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string name="yusuf";
+	string matricNumber="HR2025";
+	double score = 85.98;
+	char grade = 'A';
+	cout<< "The Student called "<< name<< " with Matric Number of "<< matricNumber<< " score " << score << " of grade " << grade;
+	
+}
---
+++ task13.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main(){
+	/*string name="Taiwon Mujeeb";
+	int age=21;
+	string 	Dep="web";
+	string matricNumber= "SW2025";
+	char grade='A';
+    double score= 85.98;*/
+	/*cout<<"Name: " << name << endl;
+    cout<<"Age: " << age << endl;
+    cout<<"Department: " << Dep << endl;
+    cout<<"Matric Number: " << matricNumber << endl;
+    cout<<"Grade: " << grade << endl;*/
+   /* cout << "The Student called "<< name<<" with age of "<< age<<" of department "<< Dep <<" with Matric Number of "<< matricNumber<< " score " << score << " of grade " << grade;*/
+   string name;
+   int age;
+    string Dep;
+    string matricNumber;
+    char grade;
+    double score;
+cout << "Input the name of the student: " << endl;
+cin >> name;
+cout << "Input the age of the student: " << endl;
+cin >> age;
+cout << "Input the department of the student: " << endl;
+cin >> Dep;
+cout << "Input the matric number of the student: " << endl;
+cin >> matricNumber;
+cout << "Input the grade of the student: " << endl;
+cin >> grade;
+cout << "Input the score of the student: " << endl;
+cin >> score;
+cout << "The Student called "<< name<<" with age of "<< age<<" of department "<< Dep <<" with Matric Number of "<< matricNumber<< " score " << score << " of grade " << grade;
+}
+
+
