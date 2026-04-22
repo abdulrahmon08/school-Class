bed20132aa2edb6a03b7f66f2880e42237ed5a00
abdulrahmon08 <abdulrahmonsheriff08@gmail.com>
2026-04-22 08:37:41 +0100

    update

---
+++ task14.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+		int number;
+	cout << "Enter a number"<< endl;
+	cin >> number;
+	if(number%2==0){
+		cout<< "this number is even number"<< endl;
+	}
+	else{
+		cout<< "this number is odd number"<< endl; 
+	}
+	return 0;
+	}
+	
+	
+
---
+++ task16.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string name="ola";
+	int age= 15;
+	cout << "my name is "<<name<< " am "<< age<<  " years old";
+}
