d0f6cde03204627d999abaf5b772e90f33571aa0
abdulrahmon08 <abdulrahmonsheriff08@gmail.com>
2026-04-23 08:52:10 +0100

    update

---
+++ practical/bio.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string name="Abdulrahmon Sheriff Adekunle";
+    string matric_no="SW20250104467";
+    string gender="Male";
+    string date_of_birth="05th May, 2000";
+    string level="HND I";
+    string semester="First Semester";
+    string course_of_study="Software and Web Development";
+    cout << "Name: "<< name << endl;
+    cout << "Matric No: "<< matric_no << endl; 
+    cout << "Gender: "<< gender << endl;
+    cout << "Date of Birth: "<< date_of_birth << endl;
+    cout << "Level: "<< level << endl;
+    cout << "Semester: "<< semester << endl;
+    cout << "Course of Study: "<< course_of_study << endl;
+}
---
+++ practical/char.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char hash='#';
+    for(int i=0; i<=12; i++){
+        cout << hash;
+    }
+    for(int i=0; i<2; i++){
+    cout <<endl << hash << "\t\t\t" << hash << endl;
+}
+    for(int i=0; i<=12; i++){
+        cout << hash;
+    }
+return 0;
+}
